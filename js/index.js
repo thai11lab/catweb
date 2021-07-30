3f8775b07b93016b6064fff7f54d7e321c43c5e8
unknown <anhthai2000atm@gmail.com>
2021-07-30 17:00:31 +0700

    upcode

---
+++ js/index.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+    $("#slide-linh-vuc").owlCarousel({
+        items: 1,
+        center: true,
+        items: 1,
+        loop: true,
+        margin: 30,
+        nav: false,
+        autoplay: false,
+        dots: false,
+        smartSpeed: 1000,
+    });
+
+
+        $("#slider-su-kien").owlCarousel({
+            items: 1,
+            center: true,
+            items: 1,
+            loop: true,
+            margin: 30,
+            nav: false,
+            autoplay: false,
+            dots: false,
+            smartSpeed: 1000,
+        });
+
+});
